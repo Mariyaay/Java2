37cefd14b28d7b0be648765ad9cac9fe95306c92
Maria <mariya.y2802@gmail.com>
2023-11-12 22:29:37 +0200

    String compare

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str1 = scanner.nextLine();
+        String str2 = scanner.nextLine();
+
+
+        System.out.println("String 1: " + str1);
+        System.out.println("String 2: " + str2);
+
+        // Compare the two strings.
+        int result = str1.compareTo(str2);
+
+        // Display the results of the comparison.
+        if (result < 0)
+        {
+            System.out.println("\"" + str1 + "\"" +
+                " is less than " +
+                "\"" + str2 + "\"");
+        }
+        else if (result == 0)
+        {
+            System.out.println("\"" + str1 + "\"" +
+                " is equal to " +
+                "\"" + str2 + "\"");
+        }
+        else // if (result > 0)
+        {
+            System.out.println("\"" + str1 + "\"" +
+                " is greater than " +
+                "\"" + str2 + "\"");
+        }
+    }
+}
